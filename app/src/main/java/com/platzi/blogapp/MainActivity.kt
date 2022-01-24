b1fd24a3c73d0bd2851ce506f377e6c0bf65ca7b
Trilador123 <fabrix_fvz@hotmail.com>
2022-01-24 16:24:28 -0600

    firebase 2.0

--- app/src/main/java/com/platzi/blogapp/MainActivity.kt
+++ app/src/main/java/com/platzi/blogapp/MainActivity.kt
@@ -1,37 +1,82 @@
 package com.platzi.blogapp
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.ContentValues.TAG
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent?>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = FirebaseFirestore.getInstance()
-
-        //Consultar informacion
-        db.collection("ciudades").document("NY").addSnapshotListener { value, error ->
-            value?.let{ document ->
-                val ciudad = document.toObject(Ciudad::class.java)
-                Log.d("Firebase", "Color: ${ciudad?.color}")
-                Log.d("Firebase", "Population: ${ciudad?.population}")
-                Log.d("Firebase", "Postal code: ${ciudad?.pc}")
+        resultLauncher = registerForActivityResult(
+            ActivityResultContracts
+                .StartActivityForResult()
+        ){
+            if (it.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = it.data
+                val imageBitmap = data?.extras?.get("data") as Bitmap
+                imageView.setImageBitmap(imageBitmap)
+                uploadPicture(imageBitmap)
             }
-    }
+        }
 
-        //Ingresar informacion
-        db.collection("ciudades").document("LA").set(Ciudad(3000000,"Red")).addOnSuccessListener {
-            Log.d("Firebase", "Se guardo la ciudad correctamente.")
-        }.addOnFailureListener{ error ->
-            Log.e("FirebaseError", error.toString())
+        imageView = findViewById(R.id.imageView)
+
+        val takePhoto = findViewById<Button>(R.id.btn_take_picture)
+        takePhoto.setOnClickListener {
+            dispatchTakePictureIntent()
         }
     }
-}
 
-data class Ciudad(val population: Int = 0,
-                  val color: String = "",
-                  val pc: Int = 0)
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            resultLauncher.launch(takePictureIntent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "No se encontró app para tomar la foto", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun uploadPicture(bitmap: Bitmap){
+        val storageRef = FirebaseStorage.getInstance().reference
+        val imagesRef = storageRef.child("image.jpg")
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100, baos)
+        val data = baos.toByteArray()
+        val uploadTask = imagesRef.putBytes(data)
+
+        uploadTask.continueWith { task ->
+            if(!task.isSuccessful){
+                task.exception?.let { exception ->
+                    throw exception
+                }
+            }
+            imagesRef.downloadUrl
+        }.addOnCompleteListener{  task ->
+          if(task.isSuccessful){
+               val downloadURrl = task.result.toString()
+               Log.d("Store","uploadPictureURL: $downloadURrl")
+          }
+        }
+    }
+
+}
